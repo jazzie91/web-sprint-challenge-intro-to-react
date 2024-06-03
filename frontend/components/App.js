d5a474cc110b0466a611767331cfd094a405a2c7
jazzie91 <164955350+jazzie91@users.noreply.github.com>
2024-06-03 13:32:33 -0500

    Update App.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,21 +1,44 @@
 import React from 'react'
 import axios from 'axios'
 import Character from './Character'
+import { useEffect, useState } from 'react'
+
 
 const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
 
-function App() {
-  // ❗ Create state to hold the data from the API
-  // ❗ Create effects to fetch the data and put it in state
-  return (
+
+    function App() {
+      const [data, setData] = useState([]);
+    
+      useEffect(() => {
+        
+        async function getData(){
+          const[peopleRes, planetsRes] = await Promise.all([axios.get(urlPeople), axios.get(urlPlanets)])
+          let people = peopleRes.data.map(person => {
+            return {
+            ...person,
+            homeworld: planetsRes.data.find(planet => planet.id === person.homeworld)}
+        });
+        setData(people)
+        }
+        getData()
+    }, 
+      []);
+      
+      
+      return (
     <div>
       <h2>Star Wars Characters</h2>
-      <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+          <p>See the README of the project for instructions on completing this challenge</p>
+        {data.map(person => 
+          <Character key={person.id} data={person}/>
+        )}
+        
     </div>
-  )
-}
+  
+  )}
+
 
 export default App
 
